1ccfdc9f8daafda0dab31144c760c20a9746b2f2
Mikael Goglichidze <goglichidze.mike@gmail.com>
2022-02-16 11:58:14 +0400

    added loader and delete

--- src/pages/components/EntriesTable.jsx
+++ src/pages/components/EntriesTable.jsx
@@ -24,7 +24,9 @@ export const EntriesTable = (props) => {
         {isLoading && <div className='absolute left-0 top-0 w-full h-full flex items-center justify-center bg-gray-400 bg-opacity-50'>
           <div className='loader'></div>
         </div>}
-          {entries.map((entry, index) => {
+
+        {entries.length && 
+          entries.map((entry, index) => {
             return <div key={index} className="bg-main flex table-row-el">
               <div className="w-16 py-2 px-3 text-center border-r border-gray-300">{index}</div>
               <div className="w-32 py-2 px-3 text-center border-r border-gray-300"><p className='bg-red-300 rounded text-dark cursor-pointer' onClick={deleteEntry(entry._id)}>Delete</p></div>
@@ -33,7 +35,9 @@ export const EntriesTable = (props) => {
               <div className="w-1/5 py-2 px-3 border-r border-gray-300">{entry.orderId}</div>
               <div className="w-1/5 py-2 px-3 border-r border-gray-300"></div>
             </div>
-          })}
+          })
+        }
+        {!entries.length && <p>Entries not found</p>}
         </div>
       </div>
   )
